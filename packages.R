4227f2658699607888c6004ed8cfe342f331bb3e
devvartpoddar <devvart123@gmail.com>
2017-04-17 12:14:24 +0200

    Crime Lit Review

--- packages.R
+++ packages.R
@@ -2,7 +2,7 @@
 rm(list = ls())
 pkgs <- c('dplyr', 'magrittr', 'methods', 'rvest', 'stringi', 'rio', 'gmailr',
   'plm', 'knitr', 'rmarkdown', 'stargazer', 'rgdal', 'ggplot2', 'ggmap', 'viridis',
-  'scales')
+  'scales', 'tidyr', 'GGally')
 
 for (p in pkgs) {
   load <- require(p, character.only = T, quietly = T)
